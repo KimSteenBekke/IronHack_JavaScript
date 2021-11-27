e0beef91908dca947ca3f5f856dc52ba827a7191
KimSteenBekke <KimSteenBekke@lego.com>
2021-11-27 16:45:59 +0100

    conditions

---
+++ conditions3.js
@@ -0,0 +1,11 @@
+let age = 15;
+let isUnderEighteen = age < 18;
+
+if (isUnderEighteen) {
+  console.log("Sorry! You can't enter :( ");
+} else {
+  console.log('Welcome to the night club!');
+}
+
+// console output:
+// 'Welcome to the night club!'
