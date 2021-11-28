cbe41eeda75ecb02f381210b69b5e57fa33df237
KimSteenBekke <KimSteenBekke@lego.com>
2021-11-28 14:48:10 +0100

    booleans

---
+++ booleans1.js
@@ -0,0 +1,15 @@
+const name = "Ironhack"; // => string
+const age = 25; // => number
+
+console.log(name);
+
+console.log(age);
+
+console.log(name, age);
+
+let powerOn = true; // => boolean
+let isRaining = false; // => boolean
+
+2 == "2"; // => true
+2 === "2"; // => false
+2 === 2; // => true
---
+++ booleans2.js
@@ -0,0 +1,15 @@
+console.log(true && false); // ==> ? false
+console.log(11 % 3 === 2); // ==> ? true
+console.log(false || true); // ==> ? true
+console.log(!true || false); // ==> ? true
+console.log(17 == '17'); // ==> ? true
+console.log(123 === '123'); // ==> ? false
+
+//Falsy
+
+0; // <-- The Number zero
+(''); // <-- An empty String
+false; // <-- The Boolean value: false
+undefined;
+null;
+NaN;
